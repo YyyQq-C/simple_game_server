c8e98e4cd5229ba6f0825dd27f9ff8182f35b09e
YongQiancheng <yqc921015@gmail.com>
2019-10-29 19:21:55 +0800

    添加pb依赖 及服务器编解码

--- Server/Core/src/main/java/net/tcp/coder/BinaryServerDecoder.java
+++ Server/Core/src/main/java/net/tcp/coder/BinaryServerDecoder.java
@@ -2,6 +2,7 @@ package net.tcp.coder;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.AttributeKey;
 
 import java.util.List;
 
@@ -14,16 +15,41 @@ import java.util.List;
  */
 public class BinaryServerDecoder extends BaseDecoder
 {
+    /** 包序列号 */
+    private static final AttributeKey<Integer> MSG_ORDER = AttributeKey.valueOf("msg_order");
+
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception
     {
         if (ctx == null || in == null)
             return;
 
-        int len = in.readInt();
-        byte[] str = new byte[len];
-        in.readBytes(str);
-        String s = new String(str);
-        out.add(s);
+        // 消息不全 继续等待
+        if (in.readableBytes() < 4)
+            return;
+
+        // 包大小
+        int len = in.getInt(4);
+        // todo 设置包最大值
+
+        if (in.readableBytes() < len)
+            return;
+
+        // 消息结构组成 （消息长度[int] + 消息序列号[int] + 消息id[int] + 消息体[bytebuf]）
+        // 消息长度 读指针移位
+        in.readInt();
+
+        // 消息虚拟号验证
+        int msgOrder = in.readInt();
+        if (ctx.channel().attr(MSG_ORDER) != null && ctx.channel().attr(MSG_ORDER).get() + 1 != msgOrder)
+        {
+            ctx.close();
+            return;
+        }
+
+        ctx.channel().attr(MSG_ORDER).set(msgOrder);
+        // 消息id
+        int msgID = in.readInt();
+
     }
 }
--- Server/Core/src/main/java/net/tcp/coder/BinaryServerEncoder.java
+++ Server/Core/src/main/java/net/tcp/coder/BinaryServerEncoder.java
@@ -2,6 +2,7 @@ package net.tcp.coder;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
+import message.BinaryMessage;
 
 /**
  * Created by YongQc
@@ -15,11 +16,18 @@ public class BinaryServerEncoder extends BaseEncoder
     @Override
     protected void encode(ChannelHandlerContext ctx, Object msg, ByteBuf out) throws Exception
     {
-        if (msg instanceof String)
+        if (msg instanceof BinaryMessage)
         {
-            byte[] bytes = ((String) msg).getBytes();
-            out.writeInt(bytes.length);
-            out.writeBytes(bytes);
+            BinaryMessage smsg = (BinaryMessage) msg;
+            // 消息结构组成 （消息长度[int + 消息id[int] + 消息体[bytebuf]）
+            ByteBuf msgBuf = smsg.writeToByteBuf();
+            out.writeInt(4 + msgBuf.writerIndex());
+            out.writeInt(smsg.getMsgId());
+            // todo 是否压缩
+            out.writeBytes(msgBuf);
+
+            // 释放
+            msgBuf.release();
         }
     }
 }
